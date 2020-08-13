df38b2e3d5e23e410fc68516f182c8dc30c0aa6b
mrrs8 <mrrs878@foxmail.com>
2020-08-13 22:34:34 +0800

    更改update.sh

--- update.sh
+++ update.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 echo "pulling image..."
-docker pull mrrs878/blog:latest
+docker pull mrrs878/dashboard:latest
 
 echo "stopping old app"
-docker container stop blog
+docker container stop dashboard
 
 echo "remove old container"
-docker container rm blog
+docker container rm dashboard
 
 echo "crete new container"
-docker container create --name blog -p 8081:80 mrrs878/blog:latest
+docker container create --name dashboard -p 8082:80 mrrs878/dashboard:latest
 
 echo "starting new app"
-docker container start blog
+docker container start dashboard
 
 echo "awesome, you succeeded!"
